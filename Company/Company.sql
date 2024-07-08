34591095f8d561e01dde9c2fd882084edea96d29
Alexandra Parra <96145566+AlexandraParra@users.noreply.github.com>
2024-07-08 14:14:12 -0300

    Criação de índices e procedures

--- Company/Company.sql
+++ Company/Company.sql
@@ -55,3 +55,134 @@ CREATE TABLE Works_On (
     FOREIGN KEY (Essn) REFERENCES Employee(Ssn) ON DELETE CASCADE,
     FOREIGN KEY (Pno) REFERENCES Project(Pnumber) ON DELETE CASCADE
 );
+
+--Procedure p/ inserir Employee
+DELIMITER $$
+
+CREATE PROCEDURE InsertEmployee (
+    IN p_Fname VARCHAR(50),
+    IN p_Minit CHAR(1),
+    IN p_Lname VARCHAR(50),
+    IN p_Ssn INT,
+    IN p_Bdate DATE,
+    IN p_Address VARCHAR(100),
+    IN p_Sex CHAR(1),
+    IN p_Salary FLOAT,
+    IN p_Super_ssn INT,
+    IN p_Dno INT
+)
+BEGIN
+    INSERT INTO Employee (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno)
+    VALUES (p_Fname, p_Minit, p_Lname, p_Ssn, p_Bdate, p_Address, p_Sex, p_Salary, p_Super_ssn, p_Dno);
+END$$
+
+DELIMITER ;
+
+--Procedure p/ alterar Employee
+DELIMITER $$
+
+CREATE PROCEDURE UpdateEmployee (
+    IN p_Ssn INT,
+    IN p_Fname VARCHAR(50),
+    IN p_Minit CHAR(1),
+    IN p_Lname VARCHAR(50),
+    IN p_Bdate DATE,
+    IN p_Address VARCHAR(100),
+    IN p_Sex CHAR(1),
+    IN p_Salary FLOAT,
+    IN p_Super_ssn INT,
+    IN p_Dno INT
+)
+BEGIN
+    UPDATE Employee
+    SET Fname = p_Fname, Minit = p_Minit, Lname = p_Lname, Bdate = p_Bdate,
+        Address = p_Address, Sex = p_Sex, Salary = p_Salary, Super_ssn = p_Super_ssn, Dno = p_Dno
+    WHERE Ssn = p_Ssn;
+END$$
+
+DELIMITER ;
+
+-- Procedure p/ deletar Employee
+DELIMITER $$
+
+CREATE PROCEDURE DeleteEmployee (
+    IN p_Ssn INT
+)
+BEGIN
+    DELETE FROM Employee WHERE Ssn = p_Ssn;
+END$$
+
+DELIMITER ;
+
+-- Index nome composto
+CREATE INDEX idx_employee_name ON Employee (Fname, Minit, Lname);
+
+
+-- Procedure p/ procurar Employee pelo nome composto
+DELIMITER $$
+
+CREATE PROCEDURE SelectEmployeeByName (
+    IN p_Fname VARCHAR(50),
+    IN p_Minit CHAR(1),
+    IN p_Lname VARCHAR(50)
+)
+BEGIN
+    SELECT * FROM Employee
+    WHERE Fname = p_Fname
+      AND Minit = p_Minit
+      AND Lname = p_Lname;
+END$$
+
+DELIMITER ;
+
+-- Index p/ nome do department
+CREATE INDEX idx_department_name ON Department (Dname);
+
+-- Procedure p/ consultar o department com mas employee
+DELIMITER $$
+
+CREATE PROCEDURE SelectMostPopulatedDepartmentWithNameFilter(
+    IN p_Dname VARCHAR(100)
+)
+BEGIN
+    SELECT D.Dname, COUNT(E.Ssn) AS EmployeeCount
+    FROM Department D
+    LEFT JOIN Employee E ON D.Dnumber = E.Dno
+    WHERE D.Dname LIKE CONCAT('%', p_Dname, '%')
+    GROUP BY D.Dname
+    ORDER BY EmployeeCount DESC
+    LIMIT 1;
+END$$
+
+DELIMITER ;
+
+-- Procedure p/ consultar o department por cidade
+DELIMITER $$
+
+CREATE PROCEDURE SelectDepartmentsByCity (
+    IN p_City VARCHAR(100)
+)
+BEGIN
+    SELECT D.Dname, D.Dnumber, D.Mgr_ssn, D.Mgr_start_date, D.Dlocation
+    FROM Department D
+    WHERE D.Dlocation LIKE CONCAT('%', p_City, '%');
+END$$
+
+DELIMITER ;
+
+-- Index Dno do Employee
+CREATE INDEX idx_employee_dno ON Employee (Dno);
+
+-- Procedure p/ consultar consultar a relação quantidade de employee - department
+DELIMITER $$
+
+CREATE PROCEDURE GetEmployeeCountByDepartment()
+BEGIN
+    SELECT D.Dname, COUNT(E.Ssn) AS EmployeeCount
+    FROM Department D
+    LEFT JOIN Employee E ON D.Dnumber = E.Dno
+    GROUP BY D.Dname
+    ORDER BY EmployeeCount DESC;
+END$$
+
+DELIMITER ;
